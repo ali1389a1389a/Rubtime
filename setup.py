2a06abbf2ac6c5ad38ff3cf534afc86d17afcdf8
ali1389a1389a <alisamorai2233@gmail.com>
2025-07-05 12:36:48 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rubtime",
+    version="0.1.0",
+    description="کتابخانه برای مدیریت زمان و بیو گپ / چنل روبیکا",
+    author="Ali",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
